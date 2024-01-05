dc858c8d0c10281cdd0f54053ee419c38a469d10
Dmitry <dmitry.olefir@novidea.com>
2024-01-06 00:38:28 +0200

    update logic

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
 const randomImg = document.getElementById("randomImg");
-const imageFolder = "";
 const imageCount = 9;
 
+function getRandomIndex() {
+  return Math.ceil(Math.random() * imageCount);
+}
+
 function showRandomImg() {
-  const randomIndex = Math.ceil(Math.random() * imageCount);
+  const srcArr = randomImg.src.split("/");
+
+  let currentIndex;
+  if (srcArr[srcArr.length - 1].includes("jpg")) {
+    const lastEl = srcArr[srcArr.length - 1];
+    currentIndex = parseInt(lastEl.substring(0, lastEl.indexOf(".")));
+  }
+
+  let randomIndex;
+  do {
+    randomIndex = getRandomIndex();
+  } while (currentIndex === randomIndex);
+
   const imageUrl = "images/" + randomIndex + ".jpg";
   randomImg.src = imageUrl;
 }
